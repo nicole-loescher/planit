440664b553621a29f50ea5f2ac73d349eea8617b
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-03-08 16:14:31 -0700

    added aws for signup

--- app/config.py
+++ app/config.py
@@ -8,4 +8,4 @@ class Config:
   S3_BUCKET = os.environ.get("S3_BUCKET")
   S3_KEY = os.environ.get("S3_KEY")
   S3_SECRET = os.environ.get("S3_SECRET")
-  S3_LOCATION = f"http://{S3_BUCKET}.s3-us-west-1.amazonaws.com/"
+  S3_LOCATION = f"http://{S3_BUCKET}.s3.amazonaws.com/"
