b622fedb1de9512d49a3d1cff17eafa3a8531fd3
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-03-23 14:19:29 -0700

    fixed footer color

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', color: 'white', backgroundColor: 'rgb(13, 13, 57)'}}>
+        <div style={{ backgroundColor: 'rgb(13, 13, 57)'}}>
+            <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', color: 'white'}}>
                 <p style={{ color: 'white', margin: '2px 0rem 1px 0rem'}}>Created by:</p>
                 <p style={{ color: 'white', margin: '0rem 0rem 2px 0rem' }}>Nicole Loescher</p>
             </div>
-            <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'rgb(13, 13, 57)'}}>
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
                 <a href='https://github.com/nicole-loescher' style={{ boxSizing: 'border-box', margin: '0rem 2rem 2rem 2rem'}}>
                     <GitHubIcon style={{ boxSizing: 'border-box', color: 'white', fontSize: '40px' }} />
                 </a>
